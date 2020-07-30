4150efe5d5ffd67f959ed0129748a7de50ba397f
Adrico1107 <67366208+Adrico1107@users.noreply.github.com>
2020-07-30 16:24:03 +0200

    Yaaa

--- Test_plots.R
+++ Test_plots.R
@@ -1,42 +1,39 @@
-library(ggplot2)
-library(plotly)
-# Histogramme avec la courbe de distribution et ligne verticale
-
-df <- data.frame(
-  weight=round(c(rnorm(200, mean=55, sd=5),
-                 rnorm(200, mean=65, sd=5))))
-
-boot_plot <- ggplot(df, aes(x=weight)) + geom_density(alpha=.2, fill="red")+ 
-             geom_segment(aes(x=55,y=0,xend=55,yend=max(weight)),data=df,color="blue", linetype="dashed", size=1)+labs(title="Weight histogram plot",x="Weight(kg)", y = "Count")
-
-boot_plot
-
-### We have a pb with the geom_segmant function (replaces the geom_vline function with bounds)
-
-dpb <- ggplot_build(boot_plot)
-
-x1 <- min(which(dpb$data[[1]]$x >=55))
-#x2 <- max(which(dpb$data[[1]]$x <=1000000))
-
-print(dpb$data[[1]])
-
-final_plot <- boot_plot +
-  geom_area(data=data.frame(x=dpb$data[[1]]$x[x1:length(dpb$data[[1]]$x)],
-                            y=dpb$data[[1]]$y[x1:length(dpb$data[[1]]$y)]),
-            aes(x=x, y=y), fill="green",alpha=0.3) + geom_histogram(aes(y=..density..), colour="black", fill="white",alpha=0.2)
-
-ggplotly(final_plot)
+######------------------ Bootstrap results plot (using ggplot)
 
 
-### Interactive graph ###
-
-#,group=1,text=paste("Weight: ",weight))
-
-p <- ggplot(data = df, aes(x = Date, y = Revenue, group = 1,
-                           text = paste("Date: ", Date,
-                                        "<br>Revenue: $", Revenue,
-                                        "<br>Target: $", Target)
-)) +
-  geom_line(colour = "grey", aes(Date, Target)) +
-  geom_line(colour = "#408FA6")
-ggplotly(p, tooltip = "text")
+if (return_plots) {
+  
+  # Prepare the data
+  df <- data.frame(differences=boot_diff)
+  
+  boot_diff_sorted <- sort(boot_diff, decreasing = FALSE)
+  xmin <- quantile(boot_diff_sorted,alpha)
+  xmax <- quantile(boot_diff_sorted,1-alpha)
+  
+  # Density plot with vertical line (initial mean difference)
+  boot_plot <- ggplot(df, aes(x=differences)) + geom_density(alpha=.2, fill="grey")+ 
+    geom_vline(aes(xintercept=xmin),color="red", linetype="dashed", size=0.5)+geom_vline(aes(xintercept=xmax),color="green", linetype="dashed", size=0.5)+
+    geom_vline(aes(xintercept=delta_ratio_init),color="blue", linetype="solid", size=1)+
+    labs(title=title_bootstrap,x=paste0(ratio_name," differences"), y = "Density")
+  
+  # Fill in differently the areas on both sides of the vertical line and add the real histogram in the background
+  dpb <- ggplot_build(boot_plot)
+  x1 <- max(which(dpb$data[[1]]$x <=xmin))
+  x2 <- max(which(dpb$data[[1]]$x <=xmax))
+  
+  
+  # Make it interactive
+  final_plot <- ggplotly(boot_plot +
+                           geom_area(data=data.frame(x=dpb$data[[1]]$x[0:x1],
+                                                     y=dpb$data[[1]]$y[0:x1]),
+                                     aes(x=x, y=y), fill="red",alpha=0.3)+
+                           
+                           geom_area(data=data.frame(x=dpb$data[[1]]$x[x1:x2],
+                                                     y=dpb$data[[1]]$y[x1:x2]),
+                                     aes(x=x, y=y), fill="grey",alpha=0.3) +
+                           
+                           geom_area(data=data.frame(x=dpb$data[[1]]$x[x2:length(dpb$data[[1]]$x)],
+                                                     y=dpb$data[[1]]$y[x2:length(dpb$data[[1]]$x)]),
+                                     aes(x=x, y=y), fill="green",alpha=0.3) + 
+                           geom_histogram(aes(y=..density..), colour="black", fill="white",alpha=0.2))
+}
--- Test_plots_plotly.R
+++ Test_plots_plotly.R
@@ -1,30 +1,13 @@
 library(dplyr)
 library(plotly)
 
-#pal <- c("red", "blue", "green")
-#pal <- setNames(pal, c("-1", "0", "1"))
-
-#fig <- plot_ly(data = iris, x = ~Sepal.Length, y = ~Petal.Length, color = ~Species, colors = pal)
-
 alpha <- 0.05
   
-x0 <- rnorm(1000)
-x <- sort(x0, decreasing = FALSE)
+x <- rnorm(1000)
 xmin <- quantile(x,0.05)
 xmax <- quantile(x,0.95)
 
-y <- rep(0,length(x))
-for (i in 1:length(x)){
-  if (x[i]<=xmin){
-    y[i]<--1
-  }else if(x[i]>=xmax){
-    y[i]<- 1
-  }
-}
-
-df <- data.frame(norm=x,pos=y)
-
-fit <- density(df$norm)
+fit <- density(x)
 df0 <- data.frame(fitx=fit$x,fity=fit$y)
 
 
@@ -32,26 +15,42 @@ df1 <- df0 %>% filter (fitx<=xmin)
 df2 <- (df0 %>% filter (fitx>=xmin)) %>% filter (fitx<=xmax)
 df3 <- df0 %>% filter (fitx>=xmax)
 
-
-plot_ly(x=df$norm,type = "histogram", name = "Histogram : Difference \n of means in resamples")%>% 
-  add_trace(x = df1$fitx, y = df1$fity, type = "scatter", mode = "lines", fill = "tozeroy", fillcolor='rgba(255,0,0,0.5)',yaxis = "y2", name = paste0("Values in the \n",100*alpha,"th percentile")) %>% 
-  add_trace(x = df2$fitx, y = df2$fity, type = "scatter", mode = "lines", fill = "tozeroy", fillcolor='rgba(180,180,180,0.5)',yaxis = "y2",name="Middle values") %>% 
-  add_trace(x = df3$fitx, y = df3$fity, type = "scatter", mode = "lines", fill = "tozeroy", fillcolor='rgba(0,255,0,0.5)',yaxis = "y2", name = paste0("Values beyond the \n",100*(1-alpha),"th percentile")) %>% 
-  add_trace(x = list(xmin,xmin+0.000000001), y = list(0,max(df1$fity)), type = "scatter", mode = "lines", line = list(width=5, color = "red"),fill = "tozeroy", fillcolor='rgba(0,0,255,0.5)',yaxis = "y2", name = "Alpha") %>% 
-  add_trace(x = list(xmax,xmax+0.000000001), y = list(0,max(df3$fity)), type = "scatter", mode = "lines", line = list(width=5, color = "black"),fill = "tozeroy", fillcolor='rgba(0,0,255,0.5)',yaxis = "y2", name = "1-Alpha") %>% 
-  add_trace(x = list(0.5,0.5+0.000000001), y = list(0,max(df2$fity)), type = "scatter", mode = "lines", line = list(width=5, color = "green"),fill = "tozeroy",yaxis = "y2", name = "Observed inital value") %>% 
-  layout(yaxis2 = list(overlaying = "y", side = "right"))
+rangex <- max(df0$fitx)-min(df0$fitx)
+res <- hist(x,breaks=seq(from=floor(min(df1$fitx)),to=ceiling(max(df3$fitx)),by=rangex/30))
+#maxhist1 <- max(table(cut(df0$fitx,seq(min(df0$fitx),max(df0$fitx),dist(range(df0$fitx))/30)))) 
+maxhist2 <- max(res$counts)
+#print(c(maxhist1,maxhist2))
+maxdens <- max(df2$fity)
+coeff <- maxhist2/maxdens
 
 
-  add_lines(x = xmin, y=seq(0,100), name = "alpha", line = list(width=10, color = "black"), hoverinfo='x', showlegend = FALSE) %>%
-  #add_segments(x = xmin, xend = xmin, y = 0, yend = 10)%>%
-  add_annotations(x = xmin, y =10, xref = "x", yref = "y", text = "Alpha",
-                  xanchor = "right", showarrow = F, textangle=0, font = list(color="black")) %>%
-  #add_lines(x = xmax, y=seq(0,100), name = "1-alpha", line = list(width=2, color = "green"), hoverinfo='x', showlegend = FALSE) %>%
-  add_annotations(x = xmax, y =100, xref = "x", yref = "y", text = "1-Alpha",
-                  xanchor = "left", showarrow = F, textangle=0, font = list(color="black"))
+fig <- plot_ly(x=x,type = "histogram",name = "Histogram : Difference \n of means in resamples")
+fig <- fig %>% add_lines(x = xmin, y=seq(0,maxhist2/2.3), name = "alpha", line = list(width=5, color = "red"), hoverinfo='x', showlegend = FALSE) %>%
+  add_annotations(x = xmin, y =maxhist2/2, xref = "x", yref = "y", text =  paste("alpha (",alpha,")"),
+                  xanchor = "right", showarrow = F, textangle=0, font = list(color="red")) %>%
+  add_lines(x = xmax, y=seq(0,maxhist2/2.3), name = "1-alpha", line = list(width=5, color = "rgb(51, 204, 0)"), hoverinfo='x', showlegend = FALSE) %>%
+  add_annotations(x = xmax, y =maxhist2/2, xref = "x", yref = "y", text = paste("1-alpha (",1-alpha,")"),
+                  xanchor = "left", showarrow = F, textangle=0, font = list(color="rgb(51, 204, 0)"))%>%
+  add_lines(x = 0, y=seq(0,maxhist2*1.2), name = "Initial Observation", line = list(width=5, color = "rgb(255, 127, 14)",dash='dash'), hoverinfo='x', showlegend = FALSE) %>%
+  add_annotations(x = 0, y =maxhist2*1.2, xref = "x", yref = "y", text = "Initial\n Observation",
+                  xanchor = "left", showarrow = F, textangle=0, font = list(color="rgb(255, 127, 14)"))
 
 
 
+fig <- fig %>% add_trace(x = df1$fitx, y = (df1$fity)*coeff, type = "scatter",hovertemplate = paste("Density :", round(df1$fity,2),
+                                                                                                "<br> Difference of means :", round(df1$fitx,2)),
+                         mode = "lines", fill = "tozeroy", fillcolor='rgba(255,0,0,0.5)', line = list(color = 'rgb(255,0,0,0.5)', width = 4),name = paste0("Values in the \n",100*alpha,"th percentile")) %>%
+  
+  add_trace(x = df2$fitx, y = (df2$fity)*coeff, type = "scatter", hovertemplate = paste("Density :", round(df2$fity,2),
+                                                                                        "<br> Difference of means :", round(df2$fitx,2)),
+            mode = "lines", fill = "tozeroy", fillcolor='rgba(180,180,180,0.5)',line = list(color = 'rgb(180,180,180,0.5)', width = 4),name="Middle values") %>% 
+  add_trace(x = df3$fitx, y = (df3$fity)*coeff, type = "scatter", hovertemplate = paste("Density :", round(df3$fity,2),
+                                                                                         "<br> Difference of means :", round(df3$fitx,2)), 
+            mode = "lines", fill = "tozeroy", fillcolor='rgba(0,255,0,0.5)', line = list(color = 'rgb(51, 204, 0)', width = 4),name = paste0("Values beyond the \n",100*(1-alpha),"th percentile")) 
+ 
+fig <- fig %>%  layout(title="Test plot"
+                       ,legend = list(orientation="h", xanchor="center", x=0.5,y=-0.2)
+                       ,xaxis=list(title="Differences of means between samples A and B")
+                       ,yaxis=list(title="Count"))
 
-
+fig
